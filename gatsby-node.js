5adf2d3a159e6c0f6e87b52a34d63e9c15fc1ba0
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-09-10 17:20:18 -0300

    Create posts by slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // To add the slug field to each post
@@ -5,7 +6,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   // Ensures we are processing only markdown files
   if (node.internal.type === "MarkdownRemark") {
-    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+    // Use `createFilePath` to turn markdown files in our `posts`
     const slug = createFilePath({
       node,
       getNode,
@@ -19,4 +20,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${slug.slice(12)}`,
     })
   }
+}
+
+exports.createPages = ({graphql, actions}) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
 }
